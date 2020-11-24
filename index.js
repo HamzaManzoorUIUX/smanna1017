4c99eaff875a8258d5582e2b3df696b5635f7758
Hamza Manzoor <35131572+MirzaHamzaManzoor@users.noreply.github.com>
2020-11-24 11:07:38 -0800

    install mysql

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-app.use(express.urlencoded());
+const mysql=require('mysql');
 let rawdata = fs.readFileSync('json/games.json');
 let gamesNames = JSON.parse(rawdata);
 
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(express.urlencoded());
+
 // use res.render to load up an ejs view file
 
 // home page
@@ -22,15 +23,34 @@ app.get('/', function(req, res) {
 
 // about page
 app.get('/signup', function(req, res) {
-    res.render('pages/signup',{
-        games:gamesNames
-    });
+    signupCall(res,false)
 });
 app.post('/signup',function(req,res){
     if(req.method=='POST'){
-        
+// console.log(req.body);
+const firstName=checkData(req,res,'firstName')
+const lastName=checkData(req,res,'lastName')
+const emailAddress=checkData(req,res,'emailAddress')
+const password=checkData(req,res,'password')
+const termCheck=checkData(req,res,'termCheck')
     }
 })
 const port = process.env.port || 3000;
 app.listen(port);
 console.log(`server run on ${port}`);
+
+const signupCall=function(res,err){
+    res.render('pages/signup',{
+        games:gamesNames,
+        error:err
+    });
+}
+const checkData=function(req,res,myVal){
+    if(req.body[myVal])
+    {
+        return req.body[myVal]
+    }
+    else{
+        signupCall(res,false)
+    }
+}
